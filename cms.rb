ab54048d0eeb489cbbff5ed7c74061b692e1c146
Rachel Minto <rachel.j.minto@gmail.com>
2016-07-18 17:02:04 +0000

    Beginning test suite

--- cms.rb
+++ cms.rb
@@ -3,14 +3,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
+  @data_files = Dir.entries("data").reject { |file_name| file_name.start_with? ('.') }
   erb :index, layout: :layout
 end
 
-get "/test" do
-  redirect "/"
-end
-
-get "favicon.ico" do
-  redirect "/"
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
